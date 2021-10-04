489b97382aec865807adf739e5f4d0316f5cda25
Soul Agent <soul@schindler.com>
2021-10-04 12:04:40 +0200

    Update comments for install packages

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,20 @@
+#!/bin/bash
+
 #Install vim and pathogen
 sudo apt install vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#Install solarized color
 cd ~/.vim/bundle
 git clone git://github.com/altercation/vim-colors-solarized.git
+
+#Install syntastic
 cd ~/.vim/bundle && \
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+
+#Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #Install tmux
 sudo apt install tmux -y
@@ -19,14 +28,14 @@ sudo apt install nodejs -y
 
 #Install fd-find and z
 sudo apt install fd-find -y
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.z
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/z.sh
 
 #Install doxygen
 sudo apt-add-repository universe -y
 sudo apt-get update -y
 sudo apt-get install doxygen -y
 
-#install oh-my-tmux
+#Install oh-my-tmux
 git clone https://github.com/gpakosz/.tmux.git ~/.tmux
 ln -s -f .tmux/.tmux.conf
 
